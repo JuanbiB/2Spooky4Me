9c2d52d066e9edee33f6172433ff0a97d3cfb852
JuanbiB <juanbiiberretta@gmail.com>
2016-02-29 20:43:29 -0500

    let's see
    if this works

---
+++ Assets/Character.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+//using UnityEditor;
+using System.Collections;
+
+public class Character : MonoBehaviour
+{
+    private float clock;
+    GameObject colFolder;
+    private int index=1;
+    private float trailTime; //For the length of trail and time trailColliders exist
+	private bool item = false; 
+	bool right = true;
+
+    void Start()
+    {
+        trailTime = 4.0f;
+        this.gameObject.GetComponent<SpriteRenderer>().receiveShadows = true;
+        this.gameObject.GetComponent<Renderer>().receiveShadows = true;
+        this.GetComponent<SpriteRenderer>().receiveShadows = true;
+        this.GetComponent<Renderer>().receiveShadows = true;
+        
+		TrailRenderer trail = this.gameObject.AddComponent<TrailRenderer>();
+        trail.startWidth = 0.2f;
+        trail.endWidth = 1f;
+        trail.time = trailTime;
+		//trail.material = AssetDatabase.GetBuiltinExtraResource<Material> ("Default-Particle.mat");
+	
+        colFolder = new GameObject();
+        colFolder.name = "trailColliders";
+
+        
+
+        InvokeRepeating("spawnCollider", 0.01f, 0.1f);
+        
+        /*mat = GetComponent<Renderer>().material;
+        mat.mainTexture = Resources.Load<Texture2D>("tileWall");   //in case we want to add a texture for testing
+        mat.color = new Color(1, 1, 1);                                         
+        mat.shader = Shader.Find("Sprites/Default");*/
+    }
+
+    void spawnCollider()
+    {
+        GameObject colObject = new GameObject();
+        colObject.transform.parent = colFolder.transform;
+		colObject.name =  index.ToString();
+        index++;
+        colObject.transform.position = this.gameObject.transform.position;
+        BoxCollider col = colObject.AddComponent<BoxCollider>();
+        col.size = new Vector2(1f, 1f);
+        col.isTrigger = true;
+		colObject.tag = "Scent";
+
+        StartCoroutine(destroyCollider(colObject,trailTime));
+    }
+    IEnumerator destroyCollider(GameObject col, float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        Destroy(col);
+    }
+
+//	void OnCollisionEnter(Collision coll){
+//		this.GetComponent<Rigidbody>().velocity = Vector3.zero; 
+//	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		// Direction toon's facing
+		if (right == true)
+		{
+			transform.eulerAngles = new Vector3(45, 0, 0);
+		}
+		else
+		{
+			transform.eulerAngles = new Vector3(-45, 180, 0);
+		}
+
+		transform.localPosition = new Vector3(transform.localPosition.x, 0.5f, transform.localPosition.z);
+		this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+		int speed = 3;
+
+		// Moving 'Upwards' by modifing z-value +
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.localPosition += new Vector3(0, 0, 1) * speed * Time.deltaTime;
+		}
+		// Moving 'Downwards' by modifing z-value - 
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.localPosition += new Vector3(0, 0, -1) * speed * Time.deltaTime;
+		}
+
+		// Moving Right 
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			right = true;
+			transform.localPosition += new Vector3(1, 0, 0) * speed * Time.deltaTime;
+			//this.GetComponent<Rigidbody>().velocity = transform.right * Time.deltaTime * 300;
+		}
+
+		// Move Left 
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			right = false;
+			transform.localPosition += new Vector3(-1, 0, 0) * speed * Time.deltaTime;
+		}
+    }
+
+	void gotSteak(){
+		item = true;
+		GameObject GameManager = GameObject.FindGameObjectWithTag ("Game Controller");
+		GameManager.SendMessage("UpdateGUI", "Steak");
+	}
+
+	void useSteak(){
+		item = false;
+		GameObject GameManager = GameObject.FindGameObjectWithTag ("Game Controller");
+		GameManager.SendMessage("UpdateGUI", "Nothing");
+	}
+}
---
+++ Assets/FSM/TrailState.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrailState : IState 
+
+{
+	private Transform target;
+	private int index;
+	private bool start = true; 
+	private readonly Guard enemy;
+
+	public TrailState (Guard guard)
+	{
+		enemy = guard;
+	}
+
+	public void UpdateState()
+	{
+		//Look ();
+		Follow ();
+	}
+
+	public void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "Cover") {
+			enemy.distractedState.start = Time.time;
+			ToDistractedState ();
+		}
+		//Debug.Log ("Test");
+		index++;
+	}
+
+	public void ToGuardState()
+	{
+		//Can't access - Have to go through alert.
+	}
+
+
+	public void ToPatrolState()
+	{
+		enemy.currentState = enemy.patrolState;
+	}
+
+	public void ToAlertState()
+	{
+		//enemy.currentState = enemy.alertState;
+	}
+
+	public void ToChaseState()
+	{
+		enemy.currentState = enemy.chaseState;
+	}
+
+	public void ToDistractedState ()
+	{
+		enemy.currentState = enemy.distractedState;
+	}
+
+	public void ToTrailState ()
+	{
+
+	}
+
+	private void Look()
+	{
+		//TODO Max's code here.
+	}
+
+	private void Follow()
+	{
+		GameObject golist = GameObject.Find ("trailColliders");
+		//Debug.Log (golist);
+		if (start) {
+			target = enemy.patrolState.contact.transform;
+			index =  int.Parse(enemy.patrolState.contact.name) + 1;
+			start = false;
+		} else {
+			//Debug.Log (index.ToString ());
+			target = golist.transform.Find (index.ToString ()).gameObject.transform ;
+		}
+		float step = (enemy.speed + 1) * Time.deltaTime;
+		enemy.transform.position = Vector3.MoveTowards(enemy.transform.position,target.position,step);
+	}
+}
